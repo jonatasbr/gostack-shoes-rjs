25a5bce51b3349d4a9ccce29f01be134533ef35e
Jonatas Braz <jonatasbraz@gmail.com>
2019-08-05 22:03:17 -0300

    feat: styling the page Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,65 @@
 import React from 'react';
+import {
+  MdAddCircleOutline,
+  MdRemoveCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <div>Cart</div>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTDE</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/05/D12-9119-405/D12-9119-405_detalhe1.jpg?resize=280:280"
+                alt="Tênis Nike Nightgazer Masculino"
+              />
+            </td>
+            <td>
+              <strong>Tênis Nike Nightgazer Masculino</strong>
+              <span>R$ 129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 1.429,99</strong>
+            </td>
+            <td>
+              <MdDelete size={20} color="#7159c1" />
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$ 1.429,99</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
