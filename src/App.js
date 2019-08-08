8fa2afecbef974f89560daeadab7bebbe29c3772
Jonatas Braz <jonatasbraz@gmail.com>
2019-08-07 21:19:58 -0300

    project: add reactotron

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import Routes from './routes';
+import './config/ReactotronConfig';
 
 import GlobalStyles from './styles/global';
-
 import Header from './components/Header';
+import Routes from './routes';
 
 import store from './store';
 
